09806a1e0b91bdaa04b2b486a2d0237b130100ee
Ruben <35366795+WB-141@users.noreply.github.com>
2025-06-30 06:29:56 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,58 @@
+function activateCard() {
+  const overlay = document.getElementById("po-card1");
+  const default_card = document.getElementById("p-card1");
+
+  default_card.addEventListener("mouseenter", () => {
+    overlay.classList.remove("hidden");
+  });
+
+  default_card.addEventListener("mouseleave", () => {
+    overlay.classList.add("hidden");
+  });
+
+  const overlay1 = document.getElementById("po-card2");
+  const default_card1 = document.getElementById("p-card2");
+
+  default_card1.addEventListener("mouseenter", () => {
+    overlay1.classList.remove("hidden");
+  });
+
+  default_card1.addEventListener("mouseleave", () => {
+    overlay1.classList.add("hidden");
+  });
+
+  const overlay2 = document.getElementById("po-card3");
+  const default_card2 = document.getElementById("p-card3");
+
+  default_card2.addEventListener("mouseenter", () => {
+    overlay2.classList.remove("hidden");
+  });
+
+  default_card2.addEventListener("mouseleave", () => {
+    overlay2.classList.add("hidden");
+  });
+}
+
+function scrollAboutMe(){
+    const button = document.getElementById("aboutme-button")
+    const section = document.getElementById("aboutMeSection")
+
+    button.addEventListener("click", () => {
+        event.preventDefault(); 
+        section.scrollIntoView({ behavior: "smooth" });
+    })
+}
+
+function watMaakIk(){
+    const button = document.getElementById("watMaakIk-button")
+    const section = document.getElementById("watMaakIkSection")
+
+    button.addEventListener("click", () => {
+        event.preventDefault(); 
+        section.scrollIntoView({ behavior: "smooth" });
+    })
+}
+
+scrollAboutMe();
+watMaakIk();
+activateCard();
