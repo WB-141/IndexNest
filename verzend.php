1da341eef397559cfd39fee8aaffa59f75daf70c
Ruben <35366795+WB-141@users.noreply.github.com>
2025-07-03 06:01:26 +0200

    Add files via upload

--- verzend.php
+++ verzend.php
@@ -3,24 +3,32 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $naar = "info@indexnest.nl";
 
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    // Haal gebruikersgegevens op en filter
+    $reply = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $onderwerp = htmlspecialchars($_POST["onderwerp"] ?? "");
     $vraag = htmlspecialchars($_POST["vraag"] ?? "");
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Valideer e-mailadres
+    if (!filter_var($reply, FILTER_VALIDATE_EMAIL)) {
         die("Ongeldig e-mailadres.");
     }
 
-    if (preg_match("/[\r\n]/", $email) || preg_match("/[\r\n]/", $onderwerp)) {
+    // Bescherm tegen header injectie
+    if (preg_match("/[\r\n]/", $reply) || preg_match("/[\r\n]/", $onderwerp)) {
         die("Header injectie gedetecteerd.");
     }
 
-    echo "Formulier ontvangen van: $email";
+    echo "Formulier ontvangen van: $reply";
 
-    $bericht = "Bericht ontvangen van: $email\n\nOnderwerp: $onderwerp\n\nVraag:\n$vraag";
+    // Opbouw bericht
+    $bericht = "Bericht ontvangen van: $reply\n\nOnderwerp: $onderwerp\n\nVraag:\n$vraag";
 
-    $headers = "From: $email";
+    // Belangrijke aanpassing:
+    $headers  = "From: info@indexnest.nl\r\n";
+    $headers .= "Reply-To: $reply\r\n";
+    $headers .= "Content-Type: text/plain; charset=utf-8";
 
+    // Verstuur mail
     mail($naar, $onderwerp, $bericht, $headers);
 }
 ?>
